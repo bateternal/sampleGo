505c4203aa7e79004b31f188edb35fc34b0ee73f
abolfazl <bateternal@gmail.com>
2017-12-17 17:22:35 +0330

    i want more

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"net/http"
 	"log"
+	"strings"
 )
 var tpl *template.Template
 
@@ -15,8 +16,12 @@ func main(){
 	http.HandleFunc("/about",abot)
 	http.HandleFunc("/contact",cntct)
 	http.HandleFunc("/apply",aply)
+	http.HandleFunc("/index/ajab/",sayHello)
 
-	http.ListenAndServe(":8080",nil)
+	err := http.ListenAndServe(":8080",nil)
+	if err != nil{
+		panic(err)
+	}
 }
 
 
@@ -27,6 +32,7 @@ func idx(w http.ResponseWriter, req *http.Request){
 		http.Error(w,"Internal server error",http.StatusInternalServerError)
 	}
 }
+
 func abot(w http.ResponseWriter, req *http.Request) {
 	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
 	if err != nil{
@@ -34,6 +40,7 @@ func abot(w http.ResponseWriter, req *http.Request) {
 		http.Error(w,"Internal server error",http.StatusInternalServerError)
 	}
 }
+
 func cntct(w http.ResponseWriter, req *http.Request){
 	err := tpl.ExecuteTemplate(w,"contact.gohtml",nil)
 	if err != nil{
@@ -41,10 +48,18 @@ func cntct(w http.ResponseWriter, req *http.Request){
 		http.Error(w,"Internal server error",http.StatusInternalServerError)
 	}
 }
+
 func aply(w http.ResponseWriter, req *http.Request){
 	err := tpl.ExecuteTemplate(w,"apply.gohtml",nil)
 	if err != nil{
 		log.Println(err)
 		http.Error(w,"Internal server error",http.StatusInternalServerError)
 	}
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/index/ajab/")
+	message = "Hello " + message
+	w.Write([]byte(message))
 }
